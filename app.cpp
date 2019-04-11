b1b373d8608b3e14390a3053d696fba16e2eaa8e
Anssi Eteläniemi <aetelani@hotmail.com>
2019-04-11 15:54:04 +0300

    moved data storage to anonymous namespace

--- app.cpp
+++ app.cpp
@@ -2,10 +2,13 @@
 #include <set>
 #include <algorithm>
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
-static set<Resource> data;
+namespace  {
+    auto data = make_shared<set<Resource>>();
+}
 
 App::App()
 {
@@ -13,23 +16,23 @@ App::App()
 
 void App::addResource(Resource r)
 {
-    data.insert(r);
+    data->insert(r);
 }
 
 void App::deleteResource(Resource r)
 {
-    auto it = data.find(r);
-    data.erase(it);
+    auto it = data->find(r);
+    data->erase(it);
 }
 
 Resource App::getResource(ResourceKey k)
 {
-    auto it = find_if(begin(data), end(data), [&k](auto const& j){
+    auto it = find_if(begin(*data), end(*data), [&k](auto const& j){
         cout << __FUNCTION__ << ":" << j.getId() << " == " << k << endl;
         return j.getId() == k;
     });
 
-    if (it != data.end()) {
+    if (it != data->end()) {
         cout << __FUNCTION__ << "found key" << endl;
         return *it;
     } else {
